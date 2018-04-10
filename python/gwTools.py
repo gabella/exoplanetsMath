148a34be54f9b1e594467cb595982b74754a794a
William <b.gabella@vanderbilt.edu>
2018-04-10 13:19:12 -0500

    have Katies gwTools now

--- python/gwTools.py
+++ python/gwTools.py
@@ -89,7 +89,7 @@ def ggPM(nn, ee):
     if nn_error:
         print('%s: error mode number less than 1'%'ggPM')
         return(-999.0)
-
+    print(ee)
     if ee == 0:
         return(1.0)
     else:
@@ -195,11 +195,27 @@ def aNmin(ecc):
         return(2)
     else:
         return(1)
-    
 
 
+def lisa_psd():
+    '''Computes LISA sensitivity curve according to `Cornish and Robson 2018 <https://arxiv.org/pdf/1803.01944.pdf>`_
+    '''
+    freq = np.logspace(-9,1,10000)
+    # note: freq [Hz], L_arm [m], S_n [Hz^-0.5]
+    L_arm = 2.5e9
+    f_star = 19.09*1e-3
+
+    P_oms = (1.5e-11)**2*(1. + (2.0e-3/freq)**4)
+    P_acc = (3.0e-15)**2*(1. + (0.4e-3/freq)**2)*(1. + (freq/(8.0e-3))**4)
+
+    P_n = (P_oms + 2.*(1. + np.cos(freq/f_star)**2)*P_acc/(2.*np.pi*freq)**4)/L_arm**2
+    R = 3./20./(1. + 6./10.*(freq/f_star)**2)
+    S_n = P_n/R
+
+    return interp1d(freq, S_n)
+
     
-def main():
+ main():
     print('Test dateTimeStamp().')
     print( dateTimeStamp() )
     print()
